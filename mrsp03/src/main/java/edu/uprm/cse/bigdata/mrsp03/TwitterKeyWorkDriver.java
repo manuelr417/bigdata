0cc290fdc304fff0447a4ac1f76a54e947cfb8e5
manuel <manuel.rodriguez7@upr.edu>
2017-03-06 10:34:34 -0400

    Added MRSP03

---
+++ mrsp03/src/main/java/edu/uprm/cse/bigdata/mrsp03/TwitterKeyWorkDriver.java
@@ -0,0 +1,37 @@
+package edu.uprm.cse.bigdata.mrsp03;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * Created by manuel on 3/6/17.
+ */
+public class TwitterKeyWorkDriver {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: TwitterKeyWorkDriver <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = new Job();
+        job.setJarByClass(edu.uprm.cse.bigdata.mrsp03.TwitterKeyWorkDriver.class);
+        job.setJobName("Count TweetsbyUsr");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(edu.uprm.cse.bigdata.mrsp03.TwitterKeyWordMapper.class);
+        job.setReducerClass(edu.uprm.cse.bigdata.mrsp03.TwitterKeyWorkReducer.class);
+        //job.setCombinerClass(edu.uprm.cse.bigdata.mrsp02.TwitterReduceByScreenName.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
